0eace2a70702b4826ed8d5171af15c5810709c79
Bhargavi Ambati <140686868+ambatibhargavi@users.noreply.github.com>
2024-12-09 22:32:02 +0530

    Update ProjectsGrid.js

--- src/app/components/ProjectsGrid.js
+++ src/app/components/ProjectsGrid.js
@@ -25,7 +25,7 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
     };
   });
 
-  const trails = useTrail(8, {
+  const trails = useTrail(7, {
     from: { scale: 0 },
     to: { scale: 1 },
     leave: { scale: 1 },
@@ -109,6 +109,78 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
         </animated.div>
       </animated.div>
 
+      <animated.div
+        style={animatedStyles}
+        onClick={() => setCurrentGrid(GRIDS[0])}
+        className="row-start-1 lg:col-span-3 lg:row-span-3"
+      >
+        <animated.div
+          style={trails[0]}
+          className="w-full h-full p-10 bg-[#386641] border border-black flex flex-col items-center justify-center gap-3"
+        >
+          <div className="border border-neutral-900 bg-[#F2E8CF] w-fit px-5 py-3">
+            <span className="text-5xl font-bold text-[#BC4749]" id="home">
+              {name.slice(0, nameIdx).join("")}
+              <span className="inline-block mx-2 w-6 h-1 bg-[#2A9D8F] animate-pulse"></span>
+            </span>
+          </div>
+          <div className="border text-center border-neutral-900 bg-[#F2E8CF] w-fit px-5 py-2">
+            <span className="lg:text-md text-[#BC4749]">
+              {subheading.slice(0, subheadingIdx).join("")}
+              <span className="inline-block w-3 h-0.5 mx-1 bg-[#2A9D8F] animate-pulse"></span>
+            </span>
+          </div>
+        </animated.div>
+      </animated.div>
+
+      <animated.div
+        style={animatedStyles}
+        className="lg:row-span-3 lg:col-span-3"
+      >
+        <animated.div
+          style={trails[4]}
+          className="w-full h-full relative bg-[#6A994E] border border-neutral-900 flex items-center justify-center"
+        >
+          <p className="text-center text-white text-lg">
+            Project 4: This Terraform project automates Snowflake user creation using loops (for_each), reducing code duplication and improving efficiency. Users are customized with attributes like passwords, default warehouses, roles, and namespaces. It provides a scalable, maintainable solution for managing multiple users.
+          </p>
+          <div className="flex justify-center items-center w-full h-full">
+            <a
+              href="https://github.com/ambatibhargavi/terraformloops"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-6 py-2 bg-[#D8BFD8] text-black rounded-md shadow-md hover:bg-[#E6E6FA] hover:shadow-lg transition-all"
+            >
+              View Project
+            </a>
+          </div>
+        </animated.div>
+      </animated.div>
+
+      <animated.div
+        style={animatedStyles}
+        className="lg:row-span-3 lg:col-span-4"
+      >
+        <animated.div
+          style={trails[5]}
+          className="w-full h-full relative bg-[#BC4749] border border-neutral-900 flex items-center justify-center"
+        >
+          <p className="text-center text-white text-lg">
+            Project 5: This project is an interactive chatbot using the Llama 2 language model, integrated with Streamlit for the web interface and hosted via the Replicate API. Users can interact with the chatbot, select models, and customize response parameters. Llama 2 was chosen for its flexibility, allowing fine-tuning and customization. It offers greater control compared to proprietary models like ChatGPT, making it ideal for exploring open-source LLMs.
+          </p>
+          <div className="flex justify-center items-center w-full h-full">
+            <a
+              href="https://github.com/ambatibhargavi/chatbot"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-6 py-2 bg-yellow-500 text-black rounded-md shadow-md hover:bg-yellow-600 hover:shadow-lg transition-all"
+            >
+              View Project
+            </a>
+          </div>
+        </animated.div>
+      </animated.div>
+    
       <animated.div
         style={animatedStyles}
         className="lg:row-span-3 lg:col-span-4"
