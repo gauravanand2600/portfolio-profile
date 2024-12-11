8339d392070b326775fa0e298ac74b3402a5ff9a
Bhargavi Ambati <140686868+ambatibhargavi@users.noreply.github.com>
2024-12-11 16:56:43 +0530

    Update ProjectsGrid.js

--- src/app/components/ProjectsGrid.js
+++ src/app/components/ProjectsGrid.js
@@ -41,7 +41,7 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
       link: "https://github.com/ambatibhargavi/TerraformSpotify",
       bgColor: "#A7C957",
       textColor: "black",
-      buttonColor: "purple",
+      buttonColor: "wine",
     },
     {
       title: "Argo Rollouts Deployment",
