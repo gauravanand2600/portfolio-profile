a802244030b132457f6779ff32db078f82abe0f5
Bhargavi Ambati <140686868+ambatibhargavi@users.noreply.github.com>
2025-04-01 22:33:25 +0530

    Update Resume.js

--- src/app/components/Resume.js
+++ src/app/components/Resume.js
@@ -171,7 +171,9 @@ export default function Resume() {
         rel="noopener noreferrer"
       >
         <div className='cursor-pointer hover:scale-105 transition-all lg:absolute bg-[#264653] w-fit px-5 py-2 m-3 border border-black'>
-          <h2 className='font-neutral-900 text-3xl'>Resume</h2>
+          <h2 className='font-neutral-900 text-3xl font-bold text-[#E9C46A]'>
+          Resume
+        </h2>
         </div>
       </a>
     </div>
