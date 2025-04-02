1bc78fa9163696d31365cad278fc518738266619
Anand Gaurav <anandgaurav69@gmail.com>
2025-04-02 10:55:17 +0530

    Update Resume.js

--- src/app/components/Resume.js
+++ src/app/components/Resume.js
@@ -166,14 +166,12 @@ export default function Resume() {
       </svg>
 
       <a
-        href="https://drive.google.com/file/d/1eWxQz-KDU-M8pBbJYrMtyyRj14O6sYiA/view?usp=sharing"
+        href="https://drive.google.com/file/d/15Qa6XDuJebb0ZlQH2dvjMKTJhqyDG9P7/view?usp=drivesdk"
         target="_blank"
         rel="noopener noreferrer"
       >
         <div className='cursor-pointer hover:scale-105 transition-all lg:absolute bg-[#264653] w-fit px-5 py-2 m-3 border border-black'>
-          <h2 className='font-neutral-900 text-3xl font-bold text-[#E9C46A]'>
-          Resume
-        </h2>
+          <h2 className='font-neutral-900 text-3xl font-bold text-[#E9C46A]'> Resume </h2>
         </div>
       </a>
     </div>
